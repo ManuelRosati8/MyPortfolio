e4173563cfbb61f70fade52d04c2de6ee9c25d0e
Rosati Manuel <m.rosati@tecnolife.com>
2025-08-08 13:29:01 +0200

    sections

--- components/Section.tsx
+++ components/Section.tsx
@@ -12,10 +12,10 @@ const Section = ({ children, id, className = '' }: SectionProps) => {
   return (
     <motion.section
       id={id}
-      className={`container mx-auto px-6 py-20 md:py-32 ${className}`}
+      className={`container mx-auto px-6 py-14 md:py-20 ${className}`}
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.2 }}
+      viewport={{ once: false, amount: 0.2 }}
       transition={{ duration: 0.6, ease: 'easeOut' }}
     >
       {children}
